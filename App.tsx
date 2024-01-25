632b8348b80386a2eb15f986b0b40a026077a13a
Matheus-Souto <emaildopaypal007@gmail.com>
2024-01-25 18:42:49 -0300

    Configura theme

--- App.tsx
+++ App.tsx
@@ -2,14 +2,22 @@ import { NunitoSans_400Regular, NunitoSans_700Bold, useFonts } from '@expo-googl
 import { Home } from '@screens/Home';
 import { StatusBar } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import theme from 'src/theme';
+import { ThemeProvider } from 'styled-components/native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({NunitoSans_400Regular, NunitoSans_700Bold})
 
   return (
-    <View>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+      barStyle="dark-content" 
+      backgroundColor="transparent" 
+      translucent
+      />
       <Home />
       { fontsLoaded ? <Home /> : <Text>Loading</Text> }
-    </View>    
+    </ThemeProvider>
+     
   )
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,9 @@
-import { View } from "react-native";
+import { Container } from "./styles"
 
 export function Home() {
     return (
-        <View>
+        <Container>
             
-        </View>
+        </Container>
     )
 }
